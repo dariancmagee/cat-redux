01b728f99efbf6e0638f17e80a82d5baf58e49bd
dariancmagee <dariancmagee@gmail.com>
2022-04-23 14:30:32 -0400

    first commit

---
+++ src/redux/catReducer.js
@@ -0,0 +1,32 @@
+
+
+const initialState = {
+    cats: {
+        1: {
+            name: 'Taco',
+            activity: 'Sleepy'
+        }
+    }
+}
+
+export const catReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ACTION_ADD_CAT": {
+            const { name, activity } = action.payload;
+            const id =
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15);
+        return {
+            cats: {
+                ...state.cats,
+                [id]: { name, activity },
+            },
+        };
+        }
+
+        default: 
+            return state;
+    }
+
+
+}
